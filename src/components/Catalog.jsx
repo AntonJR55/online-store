57e07e6374bd387d6d723383bbd78dbc8c626fef
Andrey <komaras@belpost.by>
2023-09-27 17:11:24 +0300

    fix structure

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -215,15 +215,47 @@ const Catalog = () => {
                         </div>
                     ))}
                     <div className="pageSelection">
-                        <div className="pageSelection_next">
-                            <button onClick={nextPage}>
-                                <span>Вперед</span>
-                            </button>
+                        <div className="pageSelection_filter">
+                            <div className="pageSelection_filter__text">
+                                <span>Выводить по</span>
+                            </div>
+                            <div className="pageSelection_filter__value">
+                                <div className="pageSelection_filter__value-item">
+                                    <span>5</span>
+                                </div>
+                                <div className="pageSelection_filter__value-item">
+                                    <span>10</span>
+                                </div>
+                                <div className="pageSelection_filter__value-item">
+                                    <span>15</span>
+                                </div>
+                            </div>
                         </div>
-                        <div className="pageSelection_prev">
-                            <button onClick={prevPage}>
-                                <span>Назад</span>
-                            </button>
+                        <div className="pageSelection_transition">
+                            <div className="transition_next">
+                                <button onClick={nextPage}>
+                                    <span>Вперед</span>
+                                </button>
+                            </div>
+                            <div className="transition_pageNumbers">
+                                <div className="transition_pageNumbers-number">
+                                    <span>1</span>
+                                </div>
+                                <div className="transition_pageNumbers-number">
+                                    <span>2</span>
+                                </div>
+                                <div className="transition_pageNumbers-number">
+                                    <span>3</span>
+                                </div>
+                            </div>
+                            <div className="transition_prev">
+                                <button onClick={prevPage}>
+                                    <span>Назад</span>
+                                </button>
+                            </div>
+                            <div className="transition_showAll">
+                                <span>Показать все товары</span>
+                            </div>
                         </div>
                     </div>
                 </div>
